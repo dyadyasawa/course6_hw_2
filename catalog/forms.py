c5cc2fd111543f4f68ed06362bf4dfada662602b
dyadyasawa <dyadyasawa@gmail.com>
2024-04-16 09:41:05 +0300

    Работа над проектом.

--- catalog/forms.py
+++ catalog/forms.py
@@ -9,31 +9,41 @@ class StyleMixin(forms.ModelForm):
         super().__init__(*args, **kwargs)
 
         for field_name, field in self.fields.items():
+
+            # if field_name == 'current_version_indicator':
+            #     field.widget.attrs["class"] = "form-check-input"
+
             field.widget.attrs["class"] = "form-control"
 
 
+
 class ProductForm(StyleMixin):
+
+    forbidden_words_list = [
+        'казино',
+        'криптовалюта',
+        'крипта',
+        'биржа',
+        'дешево',
+        'бесплатно',
+        'обман',
+        'полиция',
+        'радар'
+    ]
 
     class Meta:
         model = Product
         # fields = '__all__'
         fields = ('name', 'description', 'image', 'category', 'price',)
 
+    widgets = {
+        'current_version_indicator': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+    }
+
     def clean_name(self):
         cleaned_data = self.cleaned_data['name']
 
-        forbidden_words_list = [
-            'казино',
-            'криптовалюта',
-            'крипта',
-            'биржа',
-            'дешево',
-            'бесплатно',
-            'обман',
-            'полиция',
-            'радар'
-        ]
-        for word in forbidden_words_list:
+        for word in ProductForm.forbidden_words_list:
             if word in cleaned_data.lower():
                 raise forms.ValidationError('В названии присутствуют недопустимые слова.')
         return cleaned_data
@@ -41,18 +51,8 @@ class ProductForm(StyleMixin):
     def clean_description(self):
         cleaned_data = self.cleaned_data['description']
 
-        forbidden_words_list = [
-            'казино',
-            'криптовалюта',
-            'крипта',
-            'биржа',
-            'дешево',
-            'бесплатно',
-            'обман',
-            'полиция',
-            'радар'
-        ]
-        for word in forbidden_words_list:
+
+        for word in ProductForm.forbidden_words_list:
             if word in cleaned_data.lower():
                 raise forms.ValidationError('В описании присутствуют недопустимые слова.')
         return cleaned_data
@@ -63,3 +63,7 @@ class VersionForm(StyleMixin):
     class Meta:
         model = Version
         fields = '__all__'
+        widgets = {
+            'current_version_indicator': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+
