25140760b407c0e00f33c2afebcafad694c9dbb8
dyadyasawa <dyadyasawa@gmail.com>
2024-04-19 20:30:16 +0300

    Создание новой модели User. Настройки. Создание и применение миграций.

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'catalog',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'users.User'
